101ef9c9f7e87a6d95772613b448fe00bd4c2b8d
Spikree <avimahalingpure10@gmail.com>
2024-04-25 13:03:44 +0530

    ..

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -35,4 +35,19 @@ const listFood = async (req,res) => {
     }
 }
 
-export {addFood,listFood};
+// remove food items
+const removeFood = async (req,res) => {
+    try {
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`,() => {});
+
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({success:true,message:"Food removed"})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"error"})
+    }
+}
+
+
+export {addFood,listFood,removeFood};

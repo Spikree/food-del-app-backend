823513c90129231566ecd0411232878a69a0bd5a
Avi <avimahalingpure10@gmail.com>
2024-06-24 15:46:03 +0530

    features

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -58,4 +58,20 @@ const placeOrder = async(req,res) => {
     }
 }
 
-export {placeOrder}
+const verifyOrder = async (req,res) => {
+    const {orderId,success} = req.body;
+    try {
+        if(success == "true") {
+            await orderModel.findByIdAndUpdate(orderId,{payment:true});
+            res.json({success:true,message:"paid"})
+        } else {
+            await orderModel.findByIdAndDelete(orderId);
+            res.json({success:false,message:"Not paid"});
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"error"})
+    }
+}
+
+export {placeOrder,verifyOrder}

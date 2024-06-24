f323865cb596a37808b4ff2b939ff126a656303a
Avi <avimahalingpure10@gmail.com>
2024-06-24 18:52:34 +0530

    /

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 const placeOrder = async(req,res) => {
 
     // const frontend_url = "http://localhost:5173"
-    const frontend_url = "https://food-del-front-end.vercel.app/"
+    const frontend_url = "https://food-del-front-end.vercel.app"
 
 
     try {

1027ec160557606594f16fdce81a453c08c53a4a
Avi <avimahalingpure10@gmail.com>
2024-06-26 19:42:52 +0530

    fetched user orders

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -88,4 +88,15 @@ const userOrders = async (req,res) => {
     }
 }
 
-export {placeOrder,verifyOrder,userOrders}
+// Listing orders for admin pannel
+const listOrders = async (req,res) => {
+    try {
+        const orders = await orderModel.find({});
+        res.json({success:true,data:orders})
+    } catch(error) {
+        console.log(error);
+        res.json({success:"false",message:"error"})
+    }
+}
+
+export {placeOrder,verifyOrder,userOrders,listOrders}
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,11 +1,12 @@
 import express from "express"
 import authMiddleWare from "../middleware/auth.js"
-import { placeOrder, userOrders, verifyOrder } from "../controllers/orderController.js"
+import { listOrders, placeOrder, userOrders, verifyOrder } from "../controllers/orderController.js"
 
 const orderRouter = express.Router();
 
 orderRouter.post("/place",authMiddleWare,placeOrder);
 orderRouter.post("/verify",verifyOrder);
 orderRouter.post("/userorders",authMiddleWare,userOrders);
+orderRouter.get('/list',listOrders);
 
 export default orderRouter;
